f2ae47372f3c369e75334b056be1d14849e4bac1
Salamun Nuhin <52977980+Sal47506@users.noreply.github.com>
2025-03-27 11:23:09 -0400

    stuff added

--- src/main/scala/project_2/main.scala
+++ src/main/scala/project_2/main.scala
@@ -63,28 +63,46 @@ object main{
     }
   }
 
-  class BJKSTSketch(bucket_in: Set[(String, Int)] ,  z_in: Int, bucket_size_in: Int) extends Serializable {
-/* A constructor that requies intialize the bucket and the z value. The bucket size is the bucket size of the sketch. */
-
+  class BJKSTSketch(bucket_in: Set[(String, Int)], z_in: Int, bucket_size_in: Int) extends Serializable {
     var bucket: Set[(String, Int)] = bucket_in
-    var z: Int =
+    var z: Int = z_in
+    val BJKST_bucket_size: Int = bucket_size_in
 
-    val BJKST_bucket_size = bucket_size_in;z_in
-
-    def this(s: String, z_of_s: Int, bucket_size_in: Int){
-      /* A constructor that allows you pass in a single string, zeroes of the string, and the bucket size to initialize the sketch */
-      this(Set((s, z_of_s )) , z_of_s, bucket_size_in)
+    def this(s: String, z_of_s: Int, bucket_size_in: Int) = {
+      this(Set((s, z_of_s)), z_of_s, bucket_size_in)
     }
 
-    def +(that: BJKSTSketch): BJKSTSketch = {    /* Merging two sketches */
+    def +(that: BJKSTSketch): BJKSTSketch = {
+      val combinedBucket = (this.bucket ++ that.bucket).filter { case (_, zVal) =>
+        zVal >= math.max(this.z, that.z)
+      }
 
+      val newZ = math.max(this.z, that.z)
+      val trimmedBucket = if (combinedBucket.size > BJKST_bucket_size) {
+        combinedBucket.toSeq.sortBy(-_._2).take(BJKST_bucket_size).toSet
+      } else {
+        combinedBucket
+      }
+
+      new BJKSTSketch(trimmedBucket, newZ, BJKST_bucket_size)
     }
 
-    def add_string(s: String, z_of_s: Int): BJKSTSketch = {   /* add a string to the sketch */
-
+    def add_string(s: String, z_of_s: Int): BJKSTSketch = {
+      if (z_of_s >= z) {
+        bucket += ((s, z_of_s))
+        if (bucket.size > BJKST_bucket_size) {
+          val minZ = bucket.minBy(_._2)._2
+          bucket = bucket.filter(_._2 > minZ)
+          z = bucket.map(_._2).min
+        }
+      }
+      this
     }
-  }
 
+    def estimate(): Double = {
+      math.pow(2, z) * bucket.size
+    }
+}
 
   def tidemark(x: RDD[String], trials: Int): Double = {
     val h = Seq.fill(trials)(new hash_function(2000000000))
@@ -99,14 +117,39 @@ object main{
   }
 
 
-  def BJKST(x: RDD[String], width: Int, trials: Int) : Double = {
+  def BJKST(x: RDD[String], width: Int, trials: Int): Double = {
+    val estimates = (0 until trials).map { _ =>
+      val hf = new hash_function(1L << 20)  // Assume a 2-universal hash function
+      val sketch = x.map { s =>
+        val hashed = hf.hash(s)
+        val z = Integer.numberOfTrailingZeros(hashed)
+        new BJKSTSketch(s, z, width)
+      }.reduce(_ + _)
+      sketch.estimate()
+    }
 
+    val sorted = estimates.sorted
+    if (trials % 2 == 1) sorted(trials / 2)
+    else (sorted(trials / 2 - 1) + sorted(trials / 2)) / 2.0
   }
 
 
-  def Tug_of_War(x: RDD[String], width: Int, depth:Int) : Long = {
 
+  def Tug_of_War(x: RDD[String], width: Int, depth: Int): Long = {
+    val estimates = (0 until depth).map { _ =>
+      val hashFns = Array.fill(width)(new four_universal_Rademacher_hash_function)
+      val sums = hashFns.map { h =>
+        x.map(s => h.hash(s).toLong).reduce(_ + _)
+      }
+      val means = sums.map(sum => sum * sum) // X^2 for each sketch
+      means.sum.toDouble / width
+    }
+
+    val sorted = estimates.sorted
+    if (depth % 2 == 1) sorted(depth / 2).toLong
+    else ((sorted(depth / 2 - 1) + sorted(depth / 2)) / 2.0).toLong
   }
+
 
 
   def exact_F0(x: RDD[String]) : Long = {
@@ -115,13 +158,13 @@ object main{
   }
 
 
-  def exact_F2(x: RDD[String]) : Long = {
-
+  def exact_F2(x: RDD[String]): Long = {
+    x.map(x => (x, 1)).reduceByKey(_+_).map(x => x._2*x._2).reduce(_+_)
   }
 
 
 
-  def main(args: Array[String]) {
+  def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder().appName("Project_2").getOrCreate()
 
     if(args.length < 2) {
